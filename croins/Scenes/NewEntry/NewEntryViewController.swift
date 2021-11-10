a0f74e4417755f2cb4c7e0d7de54c45cb1b90a04
Naomi Nakanishi <naomi.nknsh@gmail.com>
2021-11-10 15:30:17 -0300

    merge

--- croins/Scenes/NewEntry/NewEntryViewController.swift
+++ croins/Scenes/NewEntry/NewEntryViewController.swift
@@ -1,8 +1,45 @@
-//
-//  NewEntryViewController.swift
-//  croins
-//
-//  Created by Naomi Nakanishi on 10/11/21.
-//
+import UIKit
 
-import Foundation
+class NewEntryViewController: UIViewController {
+    
+    let pageTitle = UILabel()
+    let pageContent = UILabel()
+    
+    let spentMoneyButton = UIButton()
+    let receivedMoneyButton = UIButton()
+    
+       
+    init () {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupPageTitle()
+        view.backgroundColor = .white
+    }
+    
+    func setupPageTitle(){
+        pageTitle.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pageTitle)
+        
+        pageTitle.text = "Vamos lá"
+        pageTitle.font = UIFont.boldSystemFont(ofSize: 32)
+        pageTitle.numberOfLines = 0
+        
+        pageTitle.layout {
+            $0.topAnchor.constraint(equalTo: view.topAnchor)
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        }
+    }
+    
+    func setupConstraints() {
+        //MARK: page title constraints
+        
+    }
+    
+}
--- croins/UI/ViewCode.swift
+++ croins/UI/ViewCode.swift
@@ -5,4 +5,18 @@
 //  Created by Naomi Nakanishi on 10/11/21.
 //
 
-import Foundation
+import UIKit
+
+@resultBuilder
+struct ConstraintBuilder {
+    static func buildBlock(_ components: NSLayoutConstraint...) -> [NSLayoutConstraint] {
+        components
+    }
+}
+
+extension UIView {
+    func layout(@ConstraintBuilder using constraints: (UIView) -> [NSLayoutConstraint]) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(constraints(self))
+    }
+}
