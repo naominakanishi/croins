f93aac6b90b633da9acc1676ad71649499cb0025
Naomi Nakanishi <naomi.nknsh@gmail.com>
2021-11-21 21:45:40 -0300

    Rebuilds new entry view

---
+++ croins/Scenes/NewEntry/NewEntryView.swift
@@ -0,0 +1,195 @@
+import UIKit
+
+final class NewEntryView: UIView {
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 14
+        return view
+    }()
+    
+    private lazy var fieldsStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 16
+        return view
+    }()
+    
+    private lazy var nameTextField: UITextField = {
+        let view = TextField(inset: 15)
+        view.backgroundColor = .hex(0xF5F6F8)
+        view.attributedPlaceholder = .init(
+            string: "Nome do gasto",
+            attributes: [
+                .font : UIFont.systemFont(ofSize: 14),
+                .foregroundColor : UIColor(hex: 0x9A9696).withAlphaComponent(0.4)
+            ]
+        )
+        
+        view.font = UIFont.systemFont(ofSize: 16)
+        view.layer.cornerRadius = 10
+        view.addTarget(
+            self,
+            action: #selector(dismissKeyboard),
+            for: .editingDidEndOnExit)
+        return view
+    }()
+    
+    private lazy var coastTextField: UITextField = {
+        let view = TextField(inset: 15)
+        view.backgroundColor = UIColor(
+            hex: 0xF5F6F8)
+        view.attributedPlaceholder = .init(
+            string: "Custou",
+            attributes: [
+                .font : UIFont.systemFont(ofSize: 12),
+                .foregroundColor : UIColor(hex: 0x9A9696).withAlphaComponent(0.4)
+            ]
+        )
+        view.font = UIFont.systemFont(ofSize: 16)
+        view.layer.cornerRadius = 10
+        view.addTarget(
+            self,
+            action: #selector(dismissKeyboard),
+            for: .editingDidEndOnExit)
+        return view
+    }()
+    
+    private lazy var whenTextField: UITextField = {
+        let view = TextField(inset: 15)
+        view.backgroundColor = .hex(0xF5F6F8)
+        view.attributedPlaceholder = .init(
+            string: "Selecionar data",
+            attributes: [
+                .font : UIFont.systemFont(ofSize: 16),
+                .foregroundColor : UIColor(hex: 0x9A9696).withAlphaComponent(0.4)
+            ]
+        )
+        view.datePicker(
+            target: self,
+            doneAction: #selector(handleDonePickingDate),
+            cancelAction: #selector(handleDonePickingDidCancel))
+        return view
+    }()
+    
+    private lazy var categoriesDropdown: DropdownPicker = {
+        let view = DropdownPicker()
+        view.placeholder = "Selecionar categoria"
+        return view
+    }()
+    
+    private lazy var registerButton: UIButton = {
+        let view = UIButton()
+        view.configuration = .bordered()
+        view.configuration?.title = "Registrar gastos"
+        view.configuration?.baseForegroundColor = .blue
+        return view
+    }()
+    
+    init(
+        dropdownDataSource: DropdownDataSource,
+        frame: CGRect = .zero
+    ) {
+        super.init(frame: frame)
+        categoriesDropdown.dataSource = dropdownDataSource
+        addSubviews()
+        constraintSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        addSubview(containerView)
+        containerView.addSubview(fieldsStackView)
+        containerView.addSubview(registerButton)
+        fieldsStackView.addArrangedSubview(nameTextField)
+        fieldsStackView.addArrangedSubview(coastTextField)
+        fieldsStackView.addArrangedSubview(whenTextField)
+        fieldsStackView.addArrangedSubview(categoriesDropdown)
+    }
+    
+    func constraintSubviews() {
+        constraintContainerView()
+        constraintRegisterButton()
+        constraintFieldsStackView()
+    }
+    
+    func reloadCategories() {
+        categoriesDropdown.reloadOptions()
+    }
+}
+
+
+private extension NewEntryView {
+    func constraintContainerView() {
+        containerView.layout {
+            $0.topAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.topAnchor,
+                constant: 24)
+            $0.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: 16)
+            $0.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: -16)
+            $0.bottomAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.bottomAnchor,
+                constant: -24)
+        }
+    }
+    
+    func constraintRegisterButton() {
+        registerButton.layout {
+            $0.bottomAnchor.constraint(
+                equalTo: containerView.bottomAnchor,
+                constant: -16)
+            $0.leadingAnchor.constraint(
+                equalTo: containerView.leadingAnchor,
+                constant: 24)
+            $0.trailingAnchor.constraint(
+                equalTo: containerView.trailingAnchor,
+                constant: -16)
+        }
+    }
+    
+    func constraintFieldsStackView() {
+        fieldsStackView.layout {
+            $0.topAnchor.constraint(
+                equalTo: containerView.topAnchor,
+                constant: 16)
+            $0.leadingAnchor.constraint(
+                equalTo: containerView.leadingAnchor,
+                constant: 16)
+            $0.trailingAnchor.constraint(
+                equalTo: containerView.trailingAnchor,
+                constant: -16)
+            $0.bottomAnchor.constraint(
+                lessThanOrEqualTo: registerButton.topAnchor,
+                constant: -16)
+        }
+    }
+}
+
+@objc
+private extension NewEntryView {
+    func dismissKeyboard() {
+        endEditing(true)
+    }
+    
+    func handleDonePickingDate() {
+        guard let datePicker = whenTextField.inputView as? UIDatePicker
+        else { return }
+        let pickedDate = datePicker.date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        whenTextField.text = formatter.string(from: pickedDate)
+        whenTextField.resignFirstResponder()
+    }
+    
+    func handleDonePickingDidCancel() {
+        whenTextField.resignFirstResponder()
+    }
+}
---
+++ croins/UI/Dropdown/DropdownPicker.swift
@@ -0,0 +1,195 @@
+import UIKit
+
+final class DropdownPicker: UIView {
+    
+    enum LayoutMetrics {
+        static let margin: CGFloat = 15
+    }
+    
+    enum State {
+        case opened
+        case closed
+    }
+    
+    private var currentState: State = .closed {
+        didSet {
+            dataSource?.stateDidChange()
+        }
+    }
+    var currentSelectedIndex: Int?
+    
+    var placeholder: String? {
+        didSet {
+            guard currentSelectedIndex == nil else { return }
+            selectionLabel.text = placeholder
+        }
+    }
+    
+    weak var dataSource: DropdownDataSource?
+    
+    let selectionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Selecionar"
+        label.font = UIFont(name: "Montserrat-Regular", size: 14)
+        label.textColor = .hex(0x9A9696).withAlphaComponent(0.4)
+        label.isUserInteractionEnabled = true
+        return label
+    }()
+    
+    let chevron: UIImageView = {
+        let chevron = UIImageView()
+        chevron.image = UIImage(systemName: "chevron.down")
+        chevron.isUserInteractionEnabled = true
+        
+        return chevron
+    }()
+    
+    let headerStackView: UIStackView = {
+        let header = UIStackView()
+        header.axis = .horizontal
+        header.distribution = .fill
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.isUserInteractionEnabled = true
+        header.setContentHuggingPriority(.required, for: .horizontal)
+        return header
+    }()
+    
+    lazy var optionsTableView: UITableView = {
+        let view = SelfSizingTableView()
+        
+        view.backgroundColor = .clear
+        view.separatorStyle = .none
+        view.register(OptionCell.self, forCellReuseIdentifier: "OptionCell")
+        view.dataSource = self
+        view.delegate = self
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        addSubviews()
+        constraintSubviews()
+        configureAdditionalSettings()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        self.addSubview(headerStackView)
+        self.addSubview(optionsTableView)
+        headerStackView.addArrangedSubview(selectionLabel)
+        headerStackView.addArrangedSubview(chevron)
+    }
+    
+    private func constraintSubviews() {
+        NSLayoutConstraint.activate([
+            headerStackView.topAnchor.constraint(
+                equalTo: topAnchor,
+                constant: LayoutMetrics.margin
+            ),
+            headerStackView.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: LayoutMetrics.margin
+            ),
+            headerStackView.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: -LayoutMetrics.margin
+            ),
+            optionsTableView.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: LayoutMetrics.margin),
+            optionsTableView.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: -LayoutMetrics.margin),
+            optionsTableView.topAnchor.constraint(
+                equalTo: headerStackView.bottomAnchor,
+                constant: 0),
+            optionsTableView.bottomAnchor.constraint(
+                equalTo: bottomAnchor,
+                constant: -LayoutMetrics.margin),
+        ])
+        
+        chevron.layout {
+            $0.widthAnchor.constraint(equalToConstant: 16)
+        }
+    }
+    
+    private func configureAdditionalSettings() {
+        backgroundColor = .hex(0xF5F6F8)
+        layer.cornerRadius = 10
+        headerStackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleToggleMode)))
+    }
+    
+    @objc
+    private func handleToggleMode() {
+        if currentState == .closed {
+            render(state: .opened)
+        } else {
+            render(state: .closed)
+        }
+    }
+    
+    private func render(state: State) {
+        currentState = state
+        let targetMatrix: CGAffineTransform = state == .closed ?
+            .identity :
+            .identity.rotated(by: -.pi)
+        UIView.animate(withDuration: 0.3) {
+            self.chevron.transform = targetMatrix
+            self.optionsTableView.reloadData()
+        }
+        
+        if let currentOptionIndex = currentSelectedIndex {
+            selectionLabel.text = dataSource?.option(self, at: .init(row: currentOptionIndex, section: 0)).name
+        } else {
+            selectionLabel.text = "Selecionar"
+        }
+    }
+    
+    func reloadOptions() {
+        optionsTableView.reloadData()
+    }
+}
+
+extension DropdownPicker: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if currentState == .opened, let dataSource = dataSource {
+            return dataSource.numberOfOptions(self)
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let dataSource = dataSource else {
+            return .init()
+        }
+        guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: "OptionCell",
+                for: indexPath) as? OptionCell
+        else {
+            return .init()
+        }
+        let currentOption = dataSource.option(self, at: indexPath)
+        cell.configure(using: currentOption)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        currentSelectedIndex = indexPath.row
+        render(state: .closed)
+    }
+}
+
+extension UIScrollView {
+    func resizeScrollViewContentSize() {
+        let contentRect: CGRect = subviews.reduce(into: .zero) { rect, view in
+            rect = rect.union(view.frame)
+        }
+        contentSize = contentRect.size
+    }
+}
